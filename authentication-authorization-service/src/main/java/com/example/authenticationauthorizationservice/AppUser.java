0ce8709dbc78ecfcd65b81ffd013c060aa1cc7b5
joshNic <256jomu@gmail.com>
2022-01-27 17:07:37 -0600

    add domains

---
+++ authentication-authorization-service/src/main/java/com/example/authenticationauthorizationservice/AppUser.java
@@ -0,0 +1,25 @@
+package com.example.authenticationauthorizationservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String password;
+    private String username;
+    private String email;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> role = new ArrayList<>();
+}

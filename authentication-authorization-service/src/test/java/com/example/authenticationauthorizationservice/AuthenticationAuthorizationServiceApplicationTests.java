c0c9f6e594cc63e6ce61c9cacda3227dc0cc3686
joshNic <256jomu@gmail.com>
2022-01-27 13:50:01 -0600

    initaial commit

---
+++ authentication-authorization-service/src/test/java/com/example/authenticationauthorizationservice/AuthenticationAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticationauthorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAuthorizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

dd6d016cdc5c8221b5f92975037b7b95e846d33a
joshNic <256jomu@gmail.com>
2022-01-30 15:25:37 -0600

    add integration of auth to other services

--- authentication-authorization-service/src/main/java/com/example/authenticationauthorizationservice/configuration/SecurityConfig.java
+++ authentication-authorization-service/src/main/java/com/example/authenticationauthorizationservice/configuration/SecurityConfig.java
@@ -33,9 +33,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().ignoringAntMatchers("/eureka/**");
+//        super.configure(http);
+        http.csrf().disable();
+
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
-        http.csrf().disable();
+
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/api/login/**","/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers(GET,"/api/users/**").hasAnyAuthority("ROLE_USER");
--- blog/src/main/java/edu/miu/cs544/BlogRestController.java
+++ blog/src/main/java/edu/miu/cs544/BlogRestController.java
@@ -1,13 +1,22 @@
 package edu.miu.cs544;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
 public class BlogRestController {
 
     @Autowired
@@ -31,9 +40,18 @@ public class BlogRestController {
     }
 
     @PostMapping(path = "/blogs")
-    public RedirectView addBlogPost(@RequestBody BlogPost blogPost) {
-        blogService.addBlogPost(blogPost);
-        return new RedirectView("/api/v1/blogs/" + blogPost.getId());
+    public ResponseEntity<Object> addBlogPost(@RequestBody BlogPost blogPost, @RequestHeader HttpHeaders headers) {
+        if (headers.containsKey("Authorization")){
+
+            //Here we do logic depending on the response from auth service
+            log.info("UserLogged in, {}",blogService.getUser(headers));
+
+            blogService.addBlogPost(blogPost);
+            return new ResponseEntity<>(blogPost,HttpStatus.CREATED);
+        }
+
+        return new ResponseEntity<>(ErrorResponse.builder().message("Missing Auth Token").status("403").build(),HttpStatus.FORBIDDEN);
+
     }
 
     @DeleteMapping(path = "/blogs/{id}")
